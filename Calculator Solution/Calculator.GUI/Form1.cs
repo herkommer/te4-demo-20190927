1ba72017524c9fb52d750c54f374dc9e44344c6a
herkommer <m@herkommer.co.uk>
2019-09-27 15:35:08 +0200

    refactor: extracting method for ShowResult

--- Calculator Solution/Calculator.GUI/Form1.cs
+++ Calculator Solution/Calculator.GUI/Form1.cs
@@ -28,17 +28,22 @@ namespace Calculator.GUI
             int x = int.Parse(textBox1.Text);
             int y = int.Parse(textBox2.Text);
             int result = myCalculator.Add(x, y);
+            ShowResult(op, x, y, result);
+        }
 
+        private void ShowResult(string op, int x, int y, int result)
+        {
             listBox1.Items.Add(string.Format("{0} {3} {1} = {2}", x, y, result, op));
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
+            string op = button2.Text;
             int x = int.Parse(textBox1.Text);
             int y = int.Parse(textBox2.Text);
             int result = myCalculator.Sub(x, y);
 
-            listBox1.Items.Add(string.Format("{0} - {1} = {2}", x, y, result));
+            ShowResult(op, x, y, result);
         }
     }
 }
