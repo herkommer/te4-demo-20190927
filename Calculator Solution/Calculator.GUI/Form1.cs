e268cff7b7e38e5a8fdc70599d820e2f894b2333
herkommer <m@herkommer.co.uk>
2019-09-27 15:15:52 +0200

    first simple *wrong* way of doing the task

--- Calculator Solution/Calculator.GUI/Form1.cs
+++ Calculator Solution/Calculator.GUI/Form1.cs
@@ -15,6 +15,19 @@ namespace Calculator.GUI
         public Form1()
         {
             InitializeComponent();
+
+            button1.Text = "+";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Add operationen
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+
+            int result = x + y;
+
+            listBox1.Items.Add(string.Format("{0} + {1} = {2}", x, y, result));
         }
     }
 }
