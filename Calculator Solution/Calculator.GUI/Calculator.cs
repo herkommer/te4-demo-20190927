483ca92b82d7055de26b2076ccae686f77654d90
herkommer <m@herkommer.co.uk>
2019-09-27 15:25:18 +0200

    Added Calculator Class and refactored

---
+++ Calculator Solution/Calculator.GUI/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.GUI
+{
+    class Calculator
+    {
+        //Detta objekt ska ansvara för beräkningarna
+        //Vilka beräkningar?
+        //dvs: substraktion, addition, division, multiplikation
+
+        //Egenskaper / Properties
+
+        //Metoder
+        //Add
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        //Subtract
+        //Divide
+        //Multiply
+
+        //Händelser / Events
+    }
+}
--- Calculator Solution/Calculator.GUI/Form1.cs
+++ Calculator Solution/Calculator.GUI/Form1.cs
@@ -12,6 +12,8 @@ namespace Calculator.GUI
 {
     public partial class Form1 : Form
     {
+        Calculator myCalculator = new Calculator();
+
         public Form1()
         {
             InitializeComponent();
@@ -21,13 +23,8 @@ namespace Calculator.GUI
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            //Add operationen
-            int x = int.Parse(textBox1.Text);
-            int y = int.Parse(textBox2.Text);
-
-            int result = x + y;
-
-            listBox1.Items.Add(string.Format("{0} + {1} = {2}", x, y, result));
+            int result = myCalculator.Add(int.Parse(textBox1.Text),int.Parse(textBox2.Text));
+            listBox1.Items.Add(result);
         }
     }
 }
